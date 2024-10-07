0ec8b028aacb75bfc44cd87eca8f4900c916f295
adibfiroz <adibfiroz9702@gmail.com>
2024-10-07 20:41:18 +0530

    index update3

--- index.js
+++ index.js
@@ -22,7 +22,12 @@ app.use(
   })
 );
 
-app.options("*", cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(express.json());
 
