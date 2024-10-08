7eb9e691f1fce3159e2662b5f5ccc669d641aea4
adibfiroz <adibfiroz9702@gmail.com>
2024-10-08 14:11:00 +0530

    update dotenv

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ import Chat from "./models/chat.js";
 import UserChats from "./models/userChats.js";
 import { ClerkExpressRequireAuth } from "@clerk/clerk-sdk-node";
 import userChats from "./models/userChats.js";
+import dotenv from "dotenv";
 
 const port = process.env.PORT || 3000;
 const app = express();
+dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
